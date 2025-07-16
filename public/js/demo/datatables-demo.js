26c8780998f4276c1de886ca5dd141a2b4c376b7
Ikiyyz <rizkiyanaramdhani02@gmail.com>
2025-07-16 18:07:59 +0700

    refactor: support pie chart re-init on ajax

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,6 +1,15 @@
+function initDataTables() {
+  if ($('#dataTable').length) {
+    if ($.fn.DataTable.isDataTable('#dataTable')) {
+      $('#dataTable').DataTable().destroy();
+    }
+    $('#dataTable').DataTable({
+      pageLength: 3,
+      lengthMenu: [3, 5, 10, 25, 50, 100],
+    });
+  }
+}
+
 $(document).ready(function () {
-  $('#dataTable').DataTable({
-    pageLength: 3,
-    lengthMenu: [3, 5, 10, 25, 50, 100],
-  });
+  initDataTables();
 });
